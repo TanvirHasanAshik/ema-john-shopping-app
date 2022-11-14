a50e77f8898734839c09291530a8fa306b70b153
TanvirHasanAshik <tanvirashik41@gmail.com>
2022-11-14 13:02:29 +0600

    cart calculation complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Cart.css';
 const Cart = (props) => {
     const { cart } = props;
@@ -7,11 +7,39 @@ const Cart = (props) => {
         const value = cart[key];
         quantity = quantity + value;
     }
+    /* load data from fake db */
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    let total = 0;
+    let shipping = 0;
+    let tax = 0;
+
+    for (const cartId in cart) {
+        const cartPd = products.find(product => product.id === cartId);
+        console.log(cartPd ? cartPd : 0);
+        total = total + cartPd?.price * cart[cartId];
+    }
+    if (total >= 1000) {
+        shipping = 0;
+    } else if (total > 0 && total < 1000) {
+        shipping = 100;
+    }
+    tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
         <div className="cart">
             <h4 style={{ textAlign: 'center' }}>Order Summary</h4>
             <div className="cart-data">
                 <p>Selected Item: {quantity}</p>
+                <p>Total Price:{total}</p>
+                <p>Total Shipping Charge: {shipping}</p>
+                <p>Tax:{tax}</p>
+                <h4>Grand Total:{grandTotal}</h4>
             </div>
         </div >
     );
